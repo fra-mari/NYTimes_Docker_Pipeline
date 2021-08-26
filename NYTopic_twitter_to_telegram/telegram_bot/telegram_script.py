0e745e023c00c1eb61b86c5db014eec8174599a9
fra-mari <formicamarinaresca@gmail.com>
2021-08-26 23:07:56 +0200

    loading of the files

---
+++ NYTopic_twitter_to_telegram/telegram_bot/telegram_script.py
@@ -0,0 +1,84 @@
+""""""""""""""""""""""""""""""""""""
+"""Sends tweets to a telegram bot"""
+""""""""""""""""""""""""""""""""""""
+#built-in
+import os
+import logging
+
+from time import sleep
+
+#to pip-install
+import telegram
+from telegram.ext import Updater, dispatcher, CommandHandler, MessageHandler, Filters
+from sqlalchemy import create_engine
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)#, filename='telegram_script.log')
+
+###Environment variables
+
+TOKEN = os.getenv('TOKEN2')
+PW = os.getenv('POSTGRES_PASSWORD')  
+
+###Connect to postgres
+
+sleep(50)
+
+pg = create_engine(f'postgresql://postgres:{PW}@postgresdb:5432/nytimes', echo=False).connect()
+#pg = create_engine(f'postgresql://postgres:{PW}@localhost:5555/obama', echo=True).connect()
+
+### Extract tweets according to keyword
+
+def tweets_to_bot(words):
+    """runs a query on the Postgres database
+    #that select one tweet based on the STRING passed as argument"""
+
+    query = f"""SELECT date, text, hashtags
+        FROM tweets_transformed
+        WHERE LOWER (text) ~'[ #@.‘“"-—]*{words.lower()}[ .",;:?!—”$]+'
+        ORDER BY date DESC
+        LIMIT 1
+        ;"""
+
+    result = pg.execute(query)
+    res = list(result.fetchall())
+    if len(res) != 0:
+        
+        t = f'''✅ The New York Times last tweeted on  “<b>{words}</b>” on {str(res[0][0])}.\n\n<i>Here's the tweet:</i>\n\n{res[0][1]}\n\n<b>Hashtags</b>: <i>{res[0][2]}</i>'''
+        
+        logging.warning(f'New tweet sent to bot for input keyword: "{words}".')
+        
+        return t
+    else:
+        empty = f'''TOPIC  “<b>{words}</b>”:\nI couldn't find any tweet on this topic. 🤦🏻‍♂\n\nGive another chance to my rookie programmer and think about something else to ask me!'''
+        logging.warning(f'No tweet found for bot for input keyword: "{words}".')
+        
+        return empty
+
+### Setting up the telegram bot
+
+try:
+    updater = Updater(token=f'{TOKEN}', use_context=True)
+    logging.info('Authentication successful!')
+
+    dispatcher = updater.dispatcher
+
+
+    def start(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello folks!👋🏻\nI'm a simple bot that selects the latest tweet of the New York Times on topics of your choice.\n\nTell me a word or a string of words! I shall see if I can find it in one of the NYT tweets...", parse_mode=telegram.ParseMode.HTML)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    updater.start_polling()
+
+    def echo(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'{tweets_to_bot(update.message.text)}', parse_mode=telegram.ParseMode.HTML)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+except:
+    logging.critical('Authentication failed.')
+
---
+++ NYTopic_twitter_to_telegram/tweet_collector/get_tweets.py
@@ -0,0 +1,165 @@
+"""module to connect to Twitter API and extract tweets 
+from the New York Times account into a mangodb container."""
+
+import os
+import sys
+import logging
+from time import sleep
+from urllib3.exceptions import ReadTimeoutError
+
+import pymongo
+from tweepy import OAuthHandler, Cursor, API, Stream
+from tweepy.streaming import StreamListener
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)#, filename='get_tweets.log')
+
+
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+
+
+def authenticate():
+    """Function for handling Twitter Authentication. 
+    """
+    API_KEY = os.getenv('API_KEY')
+    API_SECRET = os.getenv('API_SECRET')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.getenv('SECRET_ACCESS_TOKEN')
+    
+    auth = OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = API(auth)
+
+    
+    return api, auth
+
+def authentication_attempt():
+    '''Try/Except pattern for authentication'''
+    try:
+        api, auth = authenticate()
+        logging.warning('Twitter authentication successful!')
+        return api, auth
+    except:
+        logging.warning('Failed Twitter authentication. Exiting.')
+        sys.exit()
+
+def cursor_cycle():
+    '''Function for reading the last 10 tweets from the NYT's Twitter timeline and sending the relevant one to the MongoDB container.'''
+        
+    for status in cursor.items(10):
+        text = status.full_text
+
+        # take extended tweets into account and rules out retweets
+
+        if 'extended_tweet' in dir(status):
+            text = status.extended_tweet.full_text
+        
+        if 'retweeted_status' in dir(status):
+            continue
+            
+        tweet = {
+            'text': text,
+            'username': status.user.screen_name,
+            'tweet_date': str(status.created_at)
+        }
+        
+        collection.insert_one(tweet)
+        logging.warning(f"Found a tweet: inserted into MongoDB: {text}")
+
+
+class MyStreamListener(StreamListener):
+    '''Class for handling the realitime twitter streaming of the NYT by sendind the relevant ones to the MongoDB container'''
+    
+    def __init__(self, max_tweets=100, *args, **kwargs):
+        # initialize the StreamListener
+        super().__init__(*args, **kwargs)
+        # set the instance attributes
+        self.max_tweets = max_tweets
+        self.counter = 0
+        self.non_relevant_counter = 0
+           
+    def on_connect(self):
+        logging.warning('Now listening for incoming tweets.')
+
+
+    def on_status(self, status):
+        """Whatever we put in this method defines what is done with
+        every single tweet as it is intercepted in real-time"""
+        
+        # increase the counter
+        self.counter += 1        
+
+        if status.user.screen_name != 'nytimes':
+            self.non_relevant_counter += 1
+
+        else: 
+            try:
+                tweet_text = status.extended_tweet["full_text"]
+            except:
+                tweet_text  = status.text
+        
+            tweet = {
+                'text': tweet_text,
+                'username': status.user.screen_name,
+                'tweet_date': str(status.created_at)
+            }
+
+            # check if we have enough tweets collected
+            if self.max_tweets == self.counter:
+                # reset the counters
+                self.counter = 0
+                logging.warning('Maximum reached. Pausing for not going into overload...')
+                logging.warning(f"Non relevant incoming tweets detected and skipped during this cycle: {self.non_relevant_counter}.")
+                self.non_relevant_counter = 0
+                # return False to stop the listener
+                return False
+            else:
+                if tweet["text"][:4] == 'RT @':
+                    self.non_relevant_counter += 1
+                else: 
+                    logging.warning(f'{tweet["tweet_date"]}: New relevant tweet arrived from NYT: {tweet["text"]}. Inserted into MongoDB.')
+                    collection.insert_one(tweet)
+    
+    def on_error(self, status):
+        if status == 420:
+            logging.critical(f'Rate limit applies. Stopping the stream for a few minutes.')
+            logging.warning(f"Non relevant incoming tweets detected and skipped during this cycle: {self.non_relevant_counter}.")
+            self.non_relevant_counter = 0
+            return False
+
+
+# we connect to the container that has mongo on it
+client = pymongo.MongoClient("mongodb")
+db = client.tweet_collector  # and we create a database on mongo called "tweet_collector"
+
+# and we create a collection called "tweet_data"
+collection = db.tweet_data
+
+#initial loading of past tweets. Runs for six hours
+api, auth = authentication_attempt()
+cursor = Cursor(api.user_timeline, id='nytimes',tweet_mode='extended')
+    
+for cycle in range(2):#10800):           
+    cursor_cycle()
+    sleep(2)
+
+#Starting the live-stream after initial loading
+while True:
+    api, auth = authentication_attempt()
+    logging.warning('Starting a streaming phase...')
+    NYTStreamListener = MyStreamListener()
+    try:
+        myStream = Stream(auth = auth, listener=NYTStreamListener, tweet_mode='extended')
+        myStream.filter(follow=["807095"])
+    except ReadTimeoutError as e:
+        logging.error(f"{e.args}: {e.message}. But this is normal after a while. Restarting the stream...")
+        continue
+    sleep(60*5)
+    logging.warning('Checking if anything relevant has been tweeted during the pause of the streaming.')
+    api, auth = authentication_attempt()
+    cursor = Cursor(api.user_timeline, id='nytimes',tweet_mode='extended')
+    cursor_cycle()
+ 
